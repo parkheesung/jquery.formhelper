0f168ed016655619b8f8128e7e3738d37f512ed0
Parkheesung <yunlang97@gmail.com>
2021-01-26 16:31:52 +0900

    selector update

--- formtojsonArray.js
+++ formtojsonArray.js
@@ -1,8 +1,7 @@
 $.fn.toJsonArray = function () {
     let formID = this.attr("id");
     let result = new Array();
-
-    $("#" + formID).children().each(function () {
+    $("#" + formID).find("nav, header, footer, label, article, section, li, ol, ul, p, a, div").each(function () {
         if ($(this).attr("data-array") != null) {
             result.push($(this).toJson());
         }
